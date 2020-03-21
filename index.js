d5ba95657020cf5d946b761c41d67857a052dbe8
Max <maxtyrrell0@gmail.com>
2020-03-21 19:36:17 +0200

    Replace bodyparser.json() with express.json()

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const PORT = process.env.PORT || 40404;
 const cors = require("cors");
-const bodyparser = require("body-parser");
 
 const app = express();
 app.use(cors());
-app.use(bodyparser.json());
+app.use(express.json());
 
 const secrets = {};
 
